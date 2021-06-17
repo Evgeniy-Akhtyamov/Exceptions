27ac8265c30076d13797b61b0d5eb96c25b80cb2
Evgeniy-Akhtyamov <Ahtyamove@mail.ru>
2021-06-17 07:18:55 +0400

    Добавьте файлы проекта.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+        public MyException(string message) : base(message)
+        { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] array = new Exception[5];
+            array[0] = new ArgumentNullException();
+            array[1] = new FormatException();
+            array[2] = new IndexOutOfRangeException();
+            array[3] = new InvalidCastException();
+            array[4] = new MyException("Произошло собственное исключение MyException");
+            try
+            {
+                Console.WriteLine("Создан массив исключений:");
+                int i = 1;
+                foreach (Exception e in array)
+                {
+                    Console.WriteLine($"{i}. {e.GetType()}");
+                    i++;
+                }
+                Console.WriteLine("Введите номер исключения");
+                int num = Convert.ToInt32(Console.ReadLine());
+                throw array[num - 1];
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine ($"Сработало исключение 1: {array[0].Message}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Сработало исключение 2: {array[1].Message}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine($"Сработало исключение 3: {array[2].Message}");
+            }
+            catch (InvalidCastException)
+            {
+                Console.WriteLine($"Сработало исключение 4: {array[3].Message}");
+            }
+            catch (MyException)
+            {
+                Console.WriteLine($"Сработало исключение 5: {array[4].Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошло непредвиденное исключение: {ex.Message}");
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lastNames = new List<string>()
+            {
+                "Иванов",
+                "Сидоров",
+                "Петров",
+                "Александров",
+                "Бобков"
+            };
+            Console.WriteLine("Дан список фамилий:");
+            foreach (var item in lastNames)
+            {
+                Console.WriteLine(item);
+            }
+            NumberReader numberReader = new NumberReader();
+            numberReader.NumberEnteredEvent += Sorter;
+            while(true)
+            {
+                try
+                {
+                    numberReader.Read(lastNames);
+                }
+                catch (MyException)
+                {
+                    Console.WriteLine("Введено некорректное значение");
+                }
+            }
+        }
+        static void Sorter(int number, List<string> list)
+        {
+            switch(number)
+            {
+                case 1: Console.WriteLine("Введено значение 1"); 
+                    list.Sort();
+                    Console.WriteLine("Выполнена сортировка в алфавитном порядке:");
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                case 2: Console.WriteLine("Введено значение 2"); 
+                    list.Sort(); 
+                    list.Reverse();
+                    Console.WriteLine("Выполнена сортировка в порядке обратном алфавитному:");
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+            }
+        }
+    }
+    class NumberReader
+    {
+        public delegate void NumberEnteredDelegate(int number, List<string> list);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+        public void Read(List<string> list)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите значение: 1 или 2");
+            string userAnswer = Console.ReadLine();
+            int number;
+            while (!int.TryParse(userAnswer, out number))
+            {
+                Console.WriteLine($"Введено не число");
+                userAnswer = Console.ReadLine();
+            }
+            if (number != 1 && number != 2) throw new MyException("Введено некорректное значение");
+            NuberEntered(number, list);
+        }
+        protected virtual void NuberEntered(int number, List<string> list)
+        {
+            NumberEnteredEvent?.Invoke(number, list);
+        }
+    }
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+        public MyException(string message) : base(message)
+        { }
+    }
+}
